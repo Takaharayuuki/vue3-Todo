17692fab3cc573e72df251a7bf56d81f52114683
Takaharayuuki <yuuki.barber@gmail.com>
2021-02-09 16:43:33 +0900

    add: new view

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,5 +1,8 @@
 <template>
-  
+  <div>
+    <input type="text">
+    <button>登録</button>
+  </div>
 </template>
 
 <script lang="ts">
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,33 @@
 <template>
-  
+  <div>
+    <ul class="todolist">
+      <li v-for="todo in state.todoList" :key="todo.ytodo">{{ todo.todo }}</li>
+    </ul>
+    <Todoinput />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
+import Todoinput from '../components/TodoInput.vue';
 
 export default defineComponent({
+  components: { Todoinput },
   name: 'TodoList',
-  
+
+  setup() {
+    const state = reactive<{ todoList: [{todo: String}, {todo: String}];}>({
+      todoList: [
+        {
+          todo: 'todo1',
+        },
+        {
+          todo: 'todo2'
+        }
+      ]
+    });
+    return { state };
+  }
 });
 </script>
 
