da6ebbcdb2852723c83bc9f338d2a6bf310f54ea
Takaharayuuki <yuuki.barber@gmail.com>
2021-02-10 18:21:47 +0900

    fix: todoが上書きされるバグを修正

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
+  <img alt="Vue logo" src="./assets/logo.png" />
   <router-view></router-view>
 </template>
 
 <script>
-import TodoList from './components/TodoList.vue'
 
 export default {
-  name: 'App',
-  components: {
-    TodoList
-  }
+  name: 'App'
 }
 </script>
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -19,13 +19,12 @@ export default defineComponent({
       if(store.state.todoList.length === 0){
         return 1
       } else {
-        const lastItem = store.state.todoList.splice(-1)[0]
+        const lastItem = store.state.todoList.slice(-1)[0]
         return lastItem.id + 1
       }
     }
 
     const add = (e) =>{
-      // this.$emit が context.emit で使用できる。
       const id = lastId()
       const value = todoRef.value
       store.commit('increment', {id, value})
