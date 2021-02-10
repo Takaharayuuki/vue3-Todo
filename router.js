e66ed9c167e1f65d45a323bc0ccf435452f2ba4c
Takaharayuuki <yuuki.barber@gmail.com>
2021-02-10 18:00:13 +0900

    add: 詳細ページ

--- router.js
+++ router.js
@@ -1,9 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 // new Router() ではなく createRouter()に変更
+import Detail from './views/Detail.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes: []
+  routes: [
+    {
+      path: '/detail/:id',
+      name: 'detail',
+      component: Detail
+    }
+  ]
 })
 
 export default router
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from '../src/store/index.ts'
+import router from '../router'
 import './index.css'
 
 const app = createApp(App)
 app.use(store)
+app.use(router)
 app.mount('#app')
